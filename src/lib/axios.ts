6b0b7727dff03baabeb18b5c5943e74d365b16ac
Gaurav Bhandari <gaurav@Gauravs-MacBook-Pro.local>
2026-01-28 00:47:00 +0530

    add cf style env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const is_dev = !!import.meta.env.DEV
+const API_BASE_URL = is_dev ? "http://localhost:8787" : (globalThis as any).BACKEND_URL
+
 const apiClient = axios.create({
-  baseURL: import.meta.env.BACKEND_URL || "http://localhost:8787",
+  baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
