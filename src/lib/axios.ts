8bd2cc0ef2d6492348c81a585235c69610d4a99d
Gaurav Bhandari <gaurav@Gauravs-MacBook-Pro.local>
2026-01-28 00:14:12 +0530

    add base url env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,15 +1,12 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:8787",
+  baseURL: import.meta.env.BACKEND_URL || "http://localhost:8787",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
   },
 });
 
-export function setBaseUrl(baseUrl: string) {
-  apiClient.defaults.baseURL = baseUrl.replace(/\/$/, "");
-}
 
 export default apiClient;
