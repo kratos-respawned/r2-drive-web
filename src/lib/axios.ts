a72d5a676b043459dddff92e964773e57040f89d
Gaurav Bhandari <gaurav@Gauravs-MacBook-Pro.local>
2026-02-02 02:00:12 +0530

    change api url var

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const is_dev = import.meta.env.DEV === true;
 export const API_BASE_URL =
-  is_dev ? "http://localhost:8787" : (globalThis as any).BACKEND_URL;
+  is_dev ? "http://localhost:8787" : (globalThis as any).VITE_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
